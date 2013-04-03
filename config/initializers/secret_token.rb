f76ccdbbb2721b14a2b6205331c93bbf9b7197e5
abigail sosa <abigail.sosa.h@gmail.com>
2013-04-03 17:06:12 -0600

    Initial commit - scaffolding

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = 'd43e3d4ce872122a4f852d0f2f63b955e59c10884525b8a92435442fe6c26e040584a9ae977e9b680ffd1d500e7c5bcfcf02c845a84ad8582d29cd0117295841'
